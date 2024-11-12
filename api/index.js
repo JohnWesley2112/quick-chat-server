f55931f068bb46ba4778ad330bfe1cbf12f43712
JohnWesley2112 <johnwesley14307@gmail.com>
2024-11-12 23:08:08 +0530

    Copied all the files from previous app

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+// module.exports = app;
+
+
+require('dotenv').config({ path: '../.env' });
+const dbConfig = require('../config/dbConfig');
+const app = require('../app');
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log('Server is up on port ', PORT);
+})
